fc66380e1408c701d983e24a9292a8adbe2ba3d2
Anthony Garreau <agarreau@dynabuy.fr>
2020-05-18 21:00:19 +0200

    Updates

---
+++ precache-manifest.0a42e6f679d68bacfc1754b8ca2dc7be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e765082a461f2ecb2aa923a021930d",
+    "url": "/stardatabase/index.html"
+  },
+  {
+    "revision": "a13adb7d3293a1ca9e84",
+    "url": "/stardatabase/static/js/2.829dfb88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/stardatabase/static/js/2.829dfb88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361286cbbb2608e2ae82",
+    "url": "/stardatabase/static/js/main.160e4d91.chunk.js"
+  },
+  {
+    "revision": "710e01d51f235e28dfa8",
+    "url": "/stardatabase/static/js/runtime-main.01cb963a.js"
+  }
+]);
